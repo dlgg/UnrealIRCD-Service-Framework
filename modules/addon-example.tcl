d96734257ef1b38ac3353581921fb738b9d6e066
Damien Lesgourgues <damien@lesgourgues.fr>
2012-01-24 20:28:22 +0100

    Little example addon for wanted structures of addons

---
+++ modules/addon-example.tcl
@@ -0,0 +1,36 @@
+#!/usr/bin/tclsh
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the Licence, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# Product name : AddonName module for UnrealIRCD Service Framework
+# Copyright (C) 2012 Damien Lesgourgues
+# Author(s): Damien Lesgourgues
+#
+##############################################################################
+puts [::msgcat::mc loadaddon "AddonName"]
+
+namespace eval addon {
+# Register Master Bot Addon
+  lappend ::irc::hook(privmsgchan) "::addon::control"
+  set ::irc::hook(privmsgchan) [::tools::nodouble $::irc::hook(privmsgchan)]
+
+# Vars for addon
+  
+}
+
+proc ::addon::control { nick chan text } {
+  # Body goes here
+}
