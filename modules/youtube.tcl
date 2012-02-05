f2e4640cb4a46b2ae8d3a77d7b96d70159113c59
Damien Lesgourgues <damien@lesgourgues.fr>
2012-02-05 19:00:18 +0100

    adding reference for average note

--- modules/youtube.tcl
+++ modules/youtube.tcl
@@ -76,7 +76,7 @@ namespace eval youtube {
         puts "Dislikes : $dislike"
       }
       ::irc::send ":$::irc::nick PRIVMSG [join [list $chan $::irc::adminchan] ,]  :$::youtube::logo \002$author\002 $title | \002Durée\002 :[::tools::duration $duration] | \002Vues\002 : $view | \002Favoris\002 : $favs"
-      ::irc::send ":$::irc::nick PRIVMSG [join [list $chan $::irc::adminchan] ,]  :$::youtube::logo \002Note moyenne\002 : $average \002par\002 $raters personnes | \002Commentaires\002 : $comms | \002J'aime\002 : $like | \002Je n'aime pas\002 : $dislike"
+      ::irc::send ":$::irc::nick PRIVMSG [join [list $chan $::irc::adminchan] ,]  :$::youtube::logo \002Note moyenne\002 : $average/5 \002par\002 $raters personnes | \002Commentaires\002 : $comms | \002J'aime\002 : $like | \002Je n'aime pas\002 : $dislike"
     }
   }
 }
