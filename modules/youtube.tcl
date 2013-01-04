ca36d1ddc138e2227ae34e188ca312fab3bd4fa5
Damien Lesgourgues <damien@lesgourgues.fr>
2013-01-04 02:11:44 +0100

    By chan configuration for youtube module

--- modules/youtube.tcl
+++ modules/youtube.tcl
@@ -29,17 +29,22 @@ http::register https 443 [list ::tls::socket -require 0]
 namespace eval youtube {
 # Register Master Bot Addon
   ::irc::hook_register privmsgchan "::youtube::control"
+  ::irc::hook_register command-youtube "::youtube::command"
 
 # Import useful procs
   namespace import ::tools::tok
 
 # Vars for addon
   set logo "\002\00301,00You\00300,04Tube\002\017"
+  set dbfile "files/youtube.db"
   set base "http://www.youtube.com"
   set api "https://gdata.youtube.com/feeds/api/videos/"
   set agent "Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.1) Gecko/2008070208 Firefox/3.0.1"
   set timeout 30000
-  set mode "full"
+  # If log is 1 then the output will be displayed on $::irc::admin chan too.
+  set log 1
+  set mode "light"
+  var db
  
   proc control { nick chan text } {
     if {$::debug==1} { puts "YouTube : " }
@@ -79,16 +84,96 @@ namespace eval youtube {
         puts "Likes    : $like"
         puts "Dislikes : $dislike"
       }
-      switch $::youtube::mode {
+      if {$::youtube::log} { set outdest [join [list $chan $::irc::admin] ,] } else { set outdest $chan }
+      switch $::youtube::db($chan) {
+        full { set outmode full }
+        light { set outmode light }
+        none { set outmode none }
+        default { set outmode $::youtube::mode }
+      }
+      switch $outmode {
         full {
-          ::irc::send ":$::irc::nick [tok PRIVMSG] [join [list $chan $::irc::admin] ,]  :$::youtube::logo \002$author\002 : $title | \002Dur?e\002 :[::tools::duration $duration] | \002Vues\002 : $view | \002Favoris\002 : $favs"
-          ::irc::send ":$::irc::nick [tok PRIVMSG] [join [list $chan $::irc::admin] ,]  :$::youtube::logo \002Note moyenne\002 : $average/5 \002par\002 $raters personnes | \002Commentaires\002 : $comms | \002J'aime\002 : $like | \002Je n'aime pas\002 : $dislike"
+          ::irc::send ":$::irc::nick [tok PRIVMSG] $outdest :$::youtube::logo \002$author\002 : $title | \002Dur?e\002 :[::tools::duration $duration] | \002Vues\002 : $view | \002Favoris\002 : $favs"
+          ::irc::send ":$::irc::nick [tok PRIVMSG] $outdest :$::youtube::logo \002Note moyenne\002 : $average/5 \002par\002 $raters personnes | \002Commentaires\002 : $comms | \002J'aime\002 : $like | \002Je n'aime pas\002 : $dislike"
         }
-        light { ::irc::send ":$::irc::nick [tok PRIVMSG] [join [list $chan $::irc::admin] ,]  :$::youtube::logo \002$author\002 : $title | \002Dur?e\002 :[::tools::duration $duration] | \002Vues\002 : $view | \002Favoris\002 : $favs | \002Note moyenne\002 : $average/5 \002par\002 $raters personnes" }
-        default { ::irc::send ":$::irc::nick [tok PRIVMSG] $::irc::admin :$::youtube::logo Bad mode for output : $::youtube::mode" }
+        light { ::irc::send ":$::irc::nick [tok PRIVMSG] $outdest :$::youtube::logo \002$author\002 : $title | \002Dur?e\002 :[::tools::duration $duration] | \002Vues\002 : $view | \002Favoris\002 : $favs | \002Note moyenne\002 : $average/5 \002par\002 $raters personnes" }
+        none { return }
+        default { ::irc::send ":$::irc::nick [tok PRIVMSG] $::irc::admin :$::youtube::logo Bad mode for output : $::youtube::db($chan)" }
       }
     }
   }
+
+  proc command { nick args } {
+    set args [join [join $args]]
+    if {$::debug} { puts "YOUTUBE : command used by $nick : [lindex $args 1] : [lrange $args 2 end]" }
+    switch [lindex $args 1] {
+      help {
+        if {$::youtube::log} { ::irc::send ":$::irc::nick [tok PRIVMSG] $::irc::adminchan :\002YOUTUBE\002 $nick : help" }
+        ::youtube::print_help $nick
+        return
+      }
+      mode {
+        set chan [string tolower [lindex $args 2]]
+        set cmdmode [string tolower [lindex $args 3]]
+        if {![::irc::is_chan $chan]} { ::irc::send ":$::irc::nick [tok NOTICE] $nick :You need to provide a chan in parameters."; return }
+        if {![::irc::is_admin $nick]} { ::irc::send ":$::irc::nick [tok NOTICE] $nick :You are not admin."; return }
+        set ::youtube::db $cmdmode
+        forcelimit $chan
+        saveDB
+        ::irc::send ":$::irc::nick [tok NOTICE] $nick :Youtube mode for $chan is now $cmdmode"
+        if {$::youtube::log} { ::irc::send ":$::irc::nick [tok PRIVMSG] $::irc::adminchan :\002YOUTUBE\002 $nick : mode $chan" }
+        return
+      }
+      show {
+        if {![::irc::is_admin $nick]} { ::irc::send ":$::irc::nick [tok NOTICE] $nick :You are not admin."; return }
+        ::irc::send ":$::irc::nick [tok NOTICE] $nick :This is the configuration of the youtube module."
+        ::irc::send ":$::irc::nick [tok NOTICE] $nick :The default configuration is $::youtube::mode"
+        ::irc::send ":$::irc::nick [tok NOTICE] $nick :Channels specific configuration"
+        foreach {index val} [array names ::youtube::db] {
+          ::irc::send ":$::irc::nick [tok NOTICE] $nick :  \002$index\002 : $val"
+        }
+        if {$::youtube::log} { ::irc::send ":$::irc::nick [tok PRIVMSG] $::irc::adminchan :\002YOUTUBE\002 $nick : show" }
+        return
+      }
+      default {
+        ::youtube::print_help $nick
+        return
+      }
+    }
+    return
+  }
+
+  proc print_help { nick } {
+    ::irc::send ":$::irc::nick [tok NOTICE] $nick :\002Youtube module help"
+    ::irc::send ":$::irc::nick [tok NOTICE] $nick : "
+    ::irc::send ":$::irc::nick [tok NOTICE] $nick :\002help\002                           : Print help"
+    ::irc::send ":$::irc::nick [tok NOTICE] $nick :\002mode\002 <#chan> <none|light|full> : Change youtube configuration for <#chan>"
+    ::irc::send ":$::irc::nick [tok NOTICE] $nick :\002show\002                           : Show the youtube configuration"
+  return
+  }
+
+  proc loadDB { } {
+    set f [open $::youtube::dbfile r]
+    set content [read -nonewline $f]
+    close $f
+    if {[info exists ::youtube::db]} { unset $::youtube::db }
+    foreach line [split $content "\n"] {
+      lappend ::youtube::db([lindex $line 0]) [lindex $line 1]
+    }
+  }
+
+  proc saveDB { } {
+    set f [open $::youtube::dbfile w]
+    foreach {index val} [array names ::youtube::db] {
+      puts "$index $val"
+      puts $f "$index $val"
+    }
+    close $f
+  }
+
 }
+
+if {![file writable $::youtube::dbfile]} { if {[file exists $::youtube::dbfile]} { puts "$::youtube::dbfile is not writable. Please correct this."; exit } else { set f [open $::youtube::dbfile w]; close $f } }
+::youtube::loadDB
 
 # vim: set fenc=utf-8 sw=2 sts=2 ts=2 et filetype=tcl
